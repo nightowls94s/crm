fab0161c43d2c2754a399092ef3ee1c26adece95
nightowls94s <69089945+nightowls94s@users.noreply.github.com>
2024-01-23 18:29:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: 'http://yourfrontenddomain.com',
